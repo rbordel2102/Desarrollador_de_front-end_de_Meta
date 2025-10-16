4a895d8a055eb2745dba458a8433bca311fdf673
rbordel2102 <rbordel2102@g.educaand.es>
2025-10-16 21:20:34 +0200

    Creacion_matrices_y_objetos

---
+++ 2.JAVASCRIPT/MATRICES,_OBJETOS_Y_FUNCIONES/Creacion_matrices_y_objetos.js
@@ -0,0 +1,32 @@
+// Ejercicio 1
+// Crea un nuevo literal de array vacío y asígnalo a la variable clothes.
+var clothes = [];
+
+// Añade 5 de tus prendas de ropa favoritas como cadenas utilizando el método push().
+clothes.push("Pantalones");
+clothes.push("Camisa");
+clothes.push("Zapatos");
+clothes.push("Calcetines");
+clothes.push("Sudadera");
+
+// Elimina la quinta pieza de ropa del array utilizando el método pop().
+clothes.pop();
+
+// Añade una nueva prenda de ropa utilizando el método push().
+clothes.push("Cinturón");
+
+// Utiliza console.log para mostrar el tercer elemento del array clothes en la consola.
+console.log(clothes[2]);
+
+// Ejercicio 2
+// Crea un nuevo objeto literal vacío y asígnalo a la variable favCar.
+var favCar = {};
+
+// Usando la notación de puntos, asigna una propiedad color al objeto favCar y dale un valor de cadena con el color de tu elección.
+favCar.color = "Rojo";
+
+// Usando la notación por puntos, asigna una propiedad convertible al objeto favCar y dale un valor booleano de tu elección.
+favCar.convertible = false;
+
+// Utiliza la consola para registrar todo el objeto favCar.
+console.log(favCar);
